5f4e1c4e5941331bd6502bc08950600d37185796
NgYiShan <flonablaze13@gmail.com>
2023-05-01 23:18:20 +0800

    did a little bit of button exercise

---
+++ button.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkinter.tix import COLUMN
+
+def life_pressed():
+    print("you got a Life!!")
+
+def death_pressed():
+    print("you got hit by truck-kun! sayonara to your next life :)")
+
+def bored_pressed():
+    print("you are bored, get a life.")
+
+def tired_pressed():
+    print("you are tired, get some sleep.")
+
+def sleep_pressed():
+    print("Sleep and don't wake up :)")
+
+main = Tk()
+main.title("Your Life Journey" )
+
+
